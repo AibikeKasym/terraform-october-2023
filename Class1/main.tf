11dc7e5413e2dbb09c9d8e35d3c4d954d76d5e33
Aibike Kasym <aibikekasymova01@gmail.com>
2024-04-03 18:26:17 -0500

    Update main.tf

--- Class1/main.tf
+++ Class1/main.tf
@@ -1,22 +1,31 @@
-resource "aws_iam_user" "lb" {
-  name = "kaizen"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_iam_user" "lb2" {
-  name = "kaizen2"
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  #key_name = aws_key_pair.deployer.key_name
+  user_data = file("apache.sh")
+
+  tags = local.common_tags
 }
 
-resource "aws_iam_group" "developers" {
-  name = "devops"
-}
-
-resource "aws_iam_group_membership" "team" {
-  name = "tf-testing-group-membership"
-
-  users = [
-    aws_iam_user.lb.name,
-    aws_iam_user.lb2.name,
-  ]
-
-  group = aws_iam_group.developers.name
+output ec2 {
+  value = aws_instance.web.public_ip
 }
